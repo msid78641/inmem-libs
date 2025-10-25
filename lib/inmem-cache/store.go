3bfff6f33fb3e55777aa4e36b536bb668ce1f7ca
vivek.yada <vivek.yada@media.net>
2025-10-25 10:47:57 +0530

    ADD base commit

--- lib/inmem-cache/store.go
+++
@@ -1,7 +0,0 @@
-package inmem_cache
-
-import "time"
-
-var UserDetailsStore *Cache = createCache(GetBigCache(), time.Second*12000)
-
-var ProductCache *Cache = createCache(GetBigCache(), time.Second*40)
